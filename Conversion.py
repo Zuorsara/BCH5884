499e8a5864906caf5c3cc7b3c3cdccd9a14d8799
Justin Lee <70180802+Zuorsara@users.noreply.github.com>
2020-09-23 01:29:26 -0400

    Update Conversion.py
    Added Repository Information

--- Conversion.py
+++ Conversion.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#https://github.com/Zuorsara/BCH5884
 
 Fahrenheit = float(input("Enter Temperature in Fahrenheit: "))
 K1 = Fahrenheit-32
@@ -7,4 +8,3 @@ K3 = K2/9
 Kelvin = K3+273.15
 
 print("The Temperature in Kelvin is: ",(Kelvin))
-
