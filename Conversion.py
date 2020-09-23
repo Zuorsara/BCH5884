21944860e49f734523901961e7d89dcc3424342b
Justin Lee <70180802+Zuorsara@users.noreply.github.com>
2020-09-22 23:57:53 -0400

    Add files via upload

---
+++ Conversion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+Fahrenheit = float(input("Enter Temperature in Fahrenheit: "))
+K1 = Fahrenheit-32
+K2 = K1*5
+K3 = K2/9
+Kelvin = K3+273.15
+
+print("The Temperature in Kelvin is: ",(Kelvin))
+
